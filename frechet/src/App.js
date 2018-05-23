766a305aaa94aa45ca10dd431a93e26e26b7d0d6
antonb10 <anton.begehr@b10.vc>
2018-05-23 08:45:11 +0200

    input list started

--- frechet/src/App.js
+++ frechet/src/App.js
@@ -2,25 +2,36 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import InputCoord from './components/InputCoord'
+import InputList from './components/InputList'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {
+        p: [
+          {x: 1, y: 1},
+          {x: 4, y: 3},
+          {x: 5, y: 2},
+          {x: 6, y: 4},
+          {x: 2, y: 8},
+        ],
+        q: [
+          {x: 2, y: 1},
+          {x: 5, y: 1},
+          {x: 6, y: 2},
+          {x: 2, y: 6},
+          {x: 5, y: 7},
+        ]
+      }
+    };
+  }
+
+  dataChanged(newData) {
+    this.setState({data: newData});
+  }
+
   render() {
-    var data = {
-      p: [
-        {x: 1, y: 1},
-        {x: 4, y: 3},
-        {x: 5, y: 2},
-        {x: 6, y: 4},
-        {x: 2, y: 8},
-      ],
-      q: [
-        {x: 2, y: 1},
-        {x: 5, y: 1},
-        {x: 6, y: 2},
-        {x: 2, y: 6},
-        {x: 5, y: 7},
-      ]
-    }
     return (
       <div className="App">
         <header className="App-header">
@@ -28,7 +39,8 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
 
-        <InputCoord data={data} size={{ width: 500, height: 500 }} />
+        <InputCoord data={this.state.data} dataChanged={this.dataChanged.bind(this)} size={{ width: 500, height: 500 }} />
+        <InputList data={this.state.data} dataChanged={this.dataChanged.bind(this)} />
 
       </div>
     );
---
+++ frechet/src/components/InputList.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import './InputList.css';
+
+import ReactList from 'react-list';
+import NumericInput from 'react-numeric-input';
+
+
+
+class InputList extends Component {
+  constructor(props){
+    super(props);
+    console.log("props: ", this.props);
+  }
+
+  renderP(index, key) {
+    return this.renderPoint(index, key, this.props.data.p[index], "p")
+  }
+
+  renderQ(index, key) {
+    return this.renderPoint(index, key, this.props.data.q[index], "q")
+  }
+
+  renderPoint(index, key, point, path) {
+    return (
+      <div key={key}>{index}: (
+        <NumericInput step={0.5} precision={2} value={point.x} snap onChange={this.onValueChange(index, key, path, "x")}/> |
+        <NumericInput step={0.5} precision={2} value={point.y} snap onChange={this.onValueChange(index, key, path, "y")}/> )
+      </div>
+    );
+  }
+
+  onValueChange(index, key, path, coord) {
+    return (valueAsNumber, valueAsString, input) => {
+      var newData = this.props.data;
+      newData[path][index][coord] = valueAsNumber;
+      this.props.dataChanged(newData);
+      console.log("newData: ", newData);
+    };
+  }
+
+  render() {
+  console.log("props: ", this.props);
+    return (
+      <div>
+        <p>Path P</p>
+        <div style={{overflow: 'auto', maxHeight: 400}}>
+          <ReactList
+            itemRenderer={this.renderP.bind(this)}
+            length={this.props.data.p.length}
+            type='uniform'
+            />
+        </div>
+        <p>Path Q</p>
+        <div style={{overflow: 'auto', maxHeight: 400}}>
+          <ReactList
+            itemRenderer={this.renderQ.bind(this)}
+            length={this.props.data.q.length}
+            type='uniform'
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default InputList;
