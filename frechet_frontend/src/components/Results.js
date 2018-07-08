fecdc7c4c8cd7ad535a2c51429e45242df5989c6
abegehr <anton.begehr@icloud.com>
2018-07-08 14:21:32 +0200

    fix new_freespace_epsilon variable declaration

--- frechet_frontend/src/components/Results.js
+++ frechet_frontend/src/components/Results.js
@@ -56,7 +56,7 @@ class Results extends Component {
   componentWillReceiveProps(nextProps) {
     let new_freespace_epsilon = 0;
     if (nextProps.data.traversals.length > 0) {
-      let new_freespace_epsilon = nextProps.data.traversals[0].epsilon[0];
+      new_freespace_epsilon = nextProps.data.traversals[0].epsilon[0];
     }
     if (this.state.freespace_epsilon !== new_freespace_epsilon) {
       this.setState({freespace_epsilon: new_freespace_epsilon});
